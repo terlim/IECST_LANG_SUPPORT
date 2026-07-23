1c59425f8a5ae8d045b3e062c930349c386552c8
terlim <terlim@yandex.ru>
2026-07-23 21:44:00 +0300

    fix: add ftplugin/st.lua to override .st filetype to iecst

---
+++ ftplugin/st.lua
@@ -0,0 +1,5 @@
+" nvim-iecst/ftplugin/st.lua
+" Executed by Neovim automatically when a .st file is opened.
+" Override the default 'st' filetype to 'iecst' so our plugin loads.
+
+vim.bo.filetype = 'iecst'
